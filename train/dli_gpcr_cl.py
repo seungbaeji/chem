c62f3a0cf8e56c3b11afd21492a1463d14a2ef9b
Ji Seungbae <seungbae@ellexi.com>
2017-08-28 17:18:48 +0900

    Neural net basic structure is uploaded

---
+++ train/dli_gpcr_cl.py
@@ -0,0 +1,9 @@
+# -*- coding: uft-8 -*
+
+from __future__ import print_function
+from __future__ import division
+import tensorflow as tf
+import numpy as np
+import logging.handlers
+import os
+import sys
